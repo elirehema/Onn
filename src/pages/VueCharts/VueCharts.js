82814c06b781ff5d5a7945fc90e9939d27ef1ead
=elirehema <=rocksandystone@gmail.com>
2019-10-18 21:21:08 +0300

    Remove NVD IMPL

--- src/pages/VueCharts/VueCharts.js
+++ src/pages/VueCharts/VueCharts.js
@@ -1,7 +1,3 @@
-
-
-
-import nv from 'nvd3';
 export default {
   name: 'add-user',
   components: {},
@@ -31,27 +27,6 @@ export default {
 
   },
   computed: {
-
-    nv.addGraph(function () {
-      var chart = nv.models.discreteBarChart()
-        .x(function (d) {
-          return d.label;
-        })
-        .y(function (d) {
-          return d.value;
-        });
-
-      d3.select('#chart svg')
-        .datum(chartdatas)
-        .call(chart);
-
-      nv.utils.windowResize(chart.update);
-
-      return chart;
-    })
-
-
-
 
   },
   mounted() {
